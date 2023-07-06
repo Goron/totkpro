3377a769084c42053b7fa712fa26065c72b19cee
Guilherme <baudolink@gmail.com>
2023-07-06 00:35:19 -0300

    V8.1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var password = "12345";
+var password = "Sv9FsdE3";
 
 function passcheck() {
     if (document.getElementById('pass1').value != password) {
@@ -6,7 +6,7 @@ function passcheck() {
         return false;
     }
     if (document.getElementById('pass1').value == password) {
-        alert('Senha Correta, Não Compartilhe!');
+        alert('Senha Correta, Nao Compartilhe!');
     }
 
 }
